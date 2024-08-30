696a36f33ff78ca1510ee0dbc28571fba9409710
Jakub Nowosad <tupiszakaczki@gmail.com>
2024-08-30 14:17:20 +0200

    adds exercises

---
+++ README.md
@@ -0,0 +1,22 @@
+# Describing and comparing spatial patterns: exercises
+
+<https://jakubnowosad.com/eon2024/>, EON Summer School 2024
+
+## Prerequisites
+
+Attendees are expected to have a working recent version or R and RStudio installed, along with several R packages listed below.
+
+- R: <https://cloud.r-project.org/>
+- RStudio: <https://posit.co/download/rstudio-desktop/#download>
+
+```r
+install.packages("remotes")
+pkg_list = c("terra", "sf", "landscapemetrics", "motif", "tidyr", "dplyr")
+remotes::install_cran(pkg_list)
+```
+
+## Exercises
+
+1. <exercises1.R>
+2. <exercises2.R>
+3. <exercises3.R>
---
+++ exercises1.R
@@ -0,0 +1,13 @@
+1. Read the data from `exdata/lc_small.tif` and visualize it. 
+What is the location of the data? 
+What are the extent of the data and its spatial resolution?
+How many categories it contains?
+2. Calculate Aggregation Index (AI) for the raster. 
+Interpret the results.
+3. Calculate Total Edge (TE) for the raster.
+Interpret the results.
+Next, read the data from `exdata/lc_small2.tif`, calculate AI and TE for this raster, and compare the results with the previous raster.
+4. Calculate Total Edge (TE) for the raster, but this time in a moving window of 9 by 9 cells.
+Visualize the results.
+5. (Extra) Using the `read_sf()` function from the **sf** package read the `exdata/points.gpkg` file.
+Next, calculate SHDI and AI of an area of 3000 meters from each sampling point (see the `sample_lsm()` function).
