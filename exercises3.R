d431ac4a13cd595d68dbd4a7fc7bbc64d0d2ee89
Jakub Nowosad <tupiszakaczki@gmail.com>
2024-09-04 16:53:36 +0200

    comments the exercises

--- exercises3.R
+++ exercises3.R
@@ -1,9 +1,9 @@
-1. Read the polygon with the study area from the `exdata/harz_borders.gpkg` file using the `read_sf()` function from the **sf** package.
-2. Read the raster data with the land cover for Europe from the `exdata/lc_europe.tif` file using the `rast()` function from the **terra** package.
-Visualize both datasets.
-3. Crop and mask the raster to the borders of the polygon.
-Visualize the results.
-4. Calculate a spatial signature for the study area. 
-Can you understand its meaning?
-5. Find which areas from the Europe raster are the most similar to the study area (it make take a minute or so).
-Try a few different window sizes (e.g., 200 or 500).
+# 1. Read the polygon with the study area from the `exdata/harz_borders.gpkg` file using the `read_sf()` function from the **sf** package.
+# 2. Read the raster data with the land cover for Europe from the `exdata/lc_europe.tif` file using the `rast()` function from the **terra** package.
+# Visualize both datasets.
+# 3. Crop and mask the raster to the borders of the polygon.
+# Visualize the results.
+# 4. Calculate a spatial signature for the study area. 
+# Can you understand its meaning?
+# 5. Find which areas from the Europe raster are the most similar to the study area (it make take a minute or so).
+# Try a few different window sizes (e.g., 200 or 500).
